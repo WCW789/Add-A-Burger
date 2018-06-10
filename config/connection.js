fd623defa528dba8cd0f64d0fa22afc0b58b2363
popstar123 <wwu3@saic.edu>
2018-06-10 03:05:01 -0500

    Changes

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,34 @@
-var mysql = require("mysql");
+ var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "w",
-  password: "qwerty",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "",
+//   password: "",
+//   database: ""
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
+
+var connection;
+if(process.env.JAWSDB_URL) {
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  
+    connection = mysql.createConnection({
+      root: 3000,
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "db_name",
+    });
+};
---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS kzu4tu6daugpxtky;
+
+CREATE DATABASE kzu4tu6daugpxtky;
+
+USE kzu4tu6daugpxtky;
+
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burgername varchar(255) NOT NULL,
+devour BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burgername, devour) VALUES ('Hamburger', true);
