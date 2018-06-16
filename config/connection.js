41e26faebc87c223606f9b99ccaa986e2134afff
popstar123 <wwu3@saic.edu>
2018-06-16 14:30:37 -0500

    Changes for Heroku

--- config/connection.js
+++ config/connection.js
@@ -25,10 +25,10 @@ if(process.env.JAWSDB_URL) {
 } else {
   
     connection = mysql.createConnection({
-      root: 3000,
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "db_name",
+      root: 3306,
+      host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "axmioyrgfd74avz3",
+      password: "ybj35pt7v4cj1s9u",
+      database: "kzu4tu6daugpxtky",
     });
 };
